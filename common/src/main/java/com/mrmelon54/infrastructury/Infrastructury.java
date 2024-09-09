cd9db060c0fe25ede99c2e80029683ff77b464b7
MrMelon54 <github@mrmelon54.com>
2024-09-09 20:57:17 +0100

    Include only config information

--- common/src/main/java/com/mrmelon54/infrastructury/Infrastructury.java
+++ common/src/main/java/com/mrmelon54/infrastructury/Infrastructury.java
@@ -2,25 +2,11 @@ package com.mrmelon54.infrastructury;
 
 import com.mrmelon54.infrastructury.utils.ConfigScreenRegistrar;
 import dev.architectury.injectables.annotations.ExpectPlatform;
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
-
-import java.util.function.Supplier;
 
 public class Infrastructury {
     public static final String MOD_ID = "infrastructury";
 
     public static void init() {
-    }
-
-    public static boolean showDebugScreen() {
-        #if MC_VER < MC_1_20_2
-        return Minecraft.getInstance().options.renderDebug;
-        #else
-        return Minecraft.getInstance().gui.getDebugOverlay().showDebugScreen();
-        #endif
     }
 
     @ExpectPlatform
--- neoforge/src/main/java/com/mrmelon54/infrastructury/neoforge/InfrastructuryImpl.java
+++ neoforge/src/main/java/com/mrmelon54/infrastructury/neoforge/InfrastructuryImpl.java
@@ -2,10 +2,18 @@ package com.mrmelon54.infrastructury.neoforge;
 
 import com.mrmelon54.infrastructury.utils.ConfigScreenRegistrar;
 import net.neoforged.fml.ModLoadingContext;
+#if MC_VER <= MC_1_20_4
 import net.neoforged.neoforge.client.ConfigScreenHandler.ConfigScreenFactory;
+#else
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+#endif
 
 public class InfrastructuryImpl {
     public static void registerConfigScreen(ConfigScreenRegistrar registrar) {
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenFactory.class,()->new ConfigScreenFactory(registrar::registerConfigScreen));
+        #if MC_VER <= MC_1_20_4
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenFactory.class, () -> new ConfigScreenFactory(registrar::registerConfigScreen));
+        #else
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> registrar::registerConfigScreen);
+        #endif
     }
 }
