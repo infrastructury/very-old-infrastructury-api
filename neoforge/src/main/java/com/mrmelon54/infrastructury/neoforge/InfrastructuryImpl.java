7f75ac0ddef708f2142661aed3a5ec1fe0fe0199
MrMelon54 <github@mrmelon54.com>
2024-12-25 22:23:03 +0000

    Rewrite neoforge impl to support new IConfigScreenFactory

--- neoforge/src/main/java/com/mrmelon54/infrastructury/neoforge/InfrastructuryImpl.java
+++ neoforge/src/main/java/com/mrmelon54/infrastructury/neoforge/InfrastructuryImpl.java
@@ -2,18 +2,23 @@ package com.mrmelon54.infrastructury.neoforge;
 
 import com.mrmelon54.infrastructury.utils.ConfigScreenRegistrar;
 import net.neoforged.fml.ModLoadingContext;
-#if MC_VER <= MC_1_20_4
-import net.neoforged.neoforge.client.ConfigScreenHandler.ConfigScreenFactory;
-#else
+#if MC_VER >= MC_1_21_1
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.minecraft.client.Minecraft;
+#elif MC_VER >= MC_1_20_6
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+#else
+import net.neoforged.neoforge.client.ConfigScreenHandler.ConfigScreenFactory;
 #endif
 
 public class InfrastructuryImpl {
     public static void registerConfigScreen(ConfigScreenRegistrar registrar) {
-        #if MC_VER <= MC_1_20_4
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenFactory.class, () -> new ConfigScreenFactory(registrar::registerConfigScreen));
-        #else
+        #if MC_VER >= MC_1_21_1
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (modContainer, parent) -> registrar.registerConfigScreen(Minecraft.getInstance(), parent));
+        #elif MC_VER >= MC_1_20_6
         ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> registrar::registerConfigScreen);
+        #else
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenFactory.class, () -> new ConfigScreenFactory(registrar::registerConfigScreen));
         #endif
     }
 }
